2ec67213ae94ba1d78e8f40ae44162fd1aa9b0af
ZhMike <zh-mike@mail.ru>
2023-02-20 20:03:26 +0500

    add hw sem_7

--- Seminar5/ex1.py
+++
@@ -1,7 +0,0 @@
-# 1. Написать программу, которая удаляет из текста все слова, содержащие "абв"
-
-text = "Я люблю абвЖава иабв Питон"
-
-answer_list = [el for el in text.split() if not "абв" in el]
-# answer_list = list(filter(lambda el: not "абв" in el, text.split()))
-print(" ".join(answer_list))
